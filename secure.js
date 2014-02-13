2b920c720aa0508d9c07d21905645e7e3726a405
MOLEDesign <Development@moledesign.biz>
2014-02-13 00:46:13 +0000

    Datafeed password

--- secure.js
+++ secure.js
@@ -1,9 +1,9 @@
 // JavaScript Document
 
-exports.username = "(username)";
-exports.password = "(password)";
+exports.username = "morgan.leecy@firstgroup.com";
+exports.password = "NCC!701e";
 
 exports.database_host ='localhost';
-exports.database_name ='';
-exports.database_username ='';
-exports.database_password ='';
+exports.database_name ='nodeRTPPM';
+exports.database_username ='nodeRTPPM';
+exports.database_password ='SP8BsvsEHwQndeA4';
