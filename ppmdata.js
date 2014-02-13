5cd5c341cc61934d05d7e13b0a1feed4d31531cf
MOLEDesign <Development@moledesign.biz>
2014-02-13 00:44:49 +0000

    Base files

---
+++ ppmdata.js
@@ -0,0 +1,45 @@
+var 	fs = require('fs'),
+    	StompClient = require('stomp-client').StompClient,
+	secure = require('./secure.js');
+	
+	
+var topic = 'RTPPM_ALL';
+var datafeed = 'datafeeds.networkrail.co.uk';
+var destination = '/topic/' + topic;
+
+// Define feed security
+var username = secure.username;
+var password = secure.password;
+
+// Define SQL connection
+
+//var connection = mysql.createConnection({
+//	host		: secure.database_host,
+//	user		: secure.database_user,
+//	password	: secure.database_password,
+//	database	: secure.database_name,
+//});
+
+
+client = new StompClient(datafeed, 61618, username, password, '1.0');
+
+client.connect(function(sessionId) {
+    console.log('Trying to connect...');
+
+	// Subscribe and collect message
+    client.subscribe(destination, function(body, headers) {
+        fs.writeFile("/home/ec2-user/datadisplay/data/ppm.json", body, function(err) {
+			if(err) {
+				console.log(err);
+			} else {
+				console.log("The file was saved!");
+				client.disconnect(function() {
+					console.log('Disconnected');
+				});
+				// Add data to MYSQL
+				console.log('Adding data to MYSQL');
+			}
+		}); 
+    });
+	// end subscription	
+});
---
+++ secure.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+exports.username = "(username)";
+exports.password = "(password)";
+
+exports.database_host ='localhost';
+exports.database_name ='';
+exports.database_username ='';
+exports.database_password ='';
